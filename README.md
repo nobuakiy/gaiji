3b03f79612eca2cc29a26866cc051e62f0c016cc
Nobuaki Yamashita <nobuakiy09@gmail.com>
2023-03-04 15:01:55 +0900

    いったん完成

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # 外字変換プログラム
 
+| ファイル名 | 内容 |
+| ---------- | ---- |
+| gaiji.py | メインプログラム |
+| filelist.txt | 入力するファイルの一覧 |
+| TRANS_MAP.csv | 外字,変換後の漢字 の形式の変換テーブル |
+| DATAFILE1.DAT | テストデータ。外字が入っているのは DATAFILE1.DATのみ |
 
+* TRANS_MAP.csv は、速度のチェックのために500件ぐらい入ってますが、文字コードとしてあり得ないものも入っています。
+* filelist.txtに、入力ファイルの一覧を書いてください。先頭が # のものは、スキップします
+
+実行例
+```DOS
+C:\Users\myname\gaiji>python gaiji.py
+```
--- gaiji.py
+++ gaiji.py
@@ -8,12 +8,12 @@ kanji_start1 = int('80', 16)
 kanji_end1   = int('9F', 16)
 kanji_start2 = int('E0', 16)
 kanji_end2   = int('FF', 16)
-gaiji_start = int('F1', 16)
+gaiji_start = int('F0', 16)
 gaiji_end   = int('F9', 16)
-CHAR_LF = b'\n'
+CHAR_LF = 13  # b'\n'
 
-# 初期設定 変数
-line_count = 1
+converted_map = {}
+not_converted_map = {}
 
 # 外字変換テーブル取得
 trans_map = {}
@@ -24,39 +24,70 @@ for line in open('TRANS_MAP.csv'):
 # ファイル名一覧取得
 file_list = []
 for fname in open('filelist.txt'):
+    if (fname.startswith('#')):
+        continue
     file_list.append(fname.rstrip('\r\n'))
-print (file_list)
 
 # 全てのファイルに対して処理
+print("ファイル名,行数,外字件数,変換不可外字件数")
 for filename in file_list:
     out_filename = 'out_' + filename
     with open(filename, "rb") as f:
+        count_gaiji = 0 # 外字の件数
+        not_converted_gaiji = 0 # 変換できなかった外字
+        count_line = 0  # 行数
         # 1M単位で、一気読み
         input_buffer = f.read(buff_length)
         output_buffer = bytearray()
-        is_kanji_first = False
+        
+        is_kanji_first = False  # 漢字の1バイト目かどうか
         while input_buffer:
             for ch in input_buffer:
+                # 前回の文字が漢字の1バイト目なら、今回は2バイト目
                 if is_kanji_first:
                     is_kanji_first = False
                     ch2 = ch
+                    # 外字の範囲かどうか
                     if gaiji_start <= ch1 and ch1 <= gaiji_end:
+                        count_gaiji += 1
                         gaiji = (ch1 << 8) + ch2
-                        converted = trans_map[gaiji] 
-                        output_buffer.append((converted >> 8) & 0xff)
-                        output_buffer.append(converted & 0xff)
-                    else:
-                        output_buffer.append(ch1)
-                        output_buffer.append(ch2)
+                        if gaiji in trans_map:
+                            converted = trans_map[gaiji] 
+                            if gaiji in converted_map:
+                                converted_map[gaiji] += 1
+                            else:
+                                converted_map[gaiji] = 1
+                            output_buffer.append((converted >> 8) & 0xff)
+                            output_buffer.append(converted & 0xff)
+                            continue
+                        else:
+                            not_converted_gaiji += 1
+                            if gaiji in converted_map:
+                                not_converted_map[gaiji] += 1
+                            else:
+                                not_converted_map[gaiji] = 1
+                    output_buffer.append(ch1)
+                    output_buffer.append(ch2)
+                # 漢字の1バイト目なら、取っておいて、2バイト目を読んだときに処理
                 elif (kanji_start1 <= ch and ch <= kanji_end1) or (kanji_start2 <= ch and ch <= kanji_end2):
                     is_kanji_first = True
                     ch1 = ch
+                # 漢字に関係ないならそのまま出力
                 else:
                     if ch == CHAR_LF:
-                        line_count += 1
+                        count_line += 1
                     output_buffer.append(ch)
+
+            print(filename, count_line, count_gaiji, not_converted_gaiji, sep=',')
             fout = open(out_filename, "wb")
             fout.write(output_buffer)
             output_buffer.clear()
             input_buffer = f.read(buff_length)
 
+print('\nconverted')
+for key in converted_map:
+    print(hex(key), ':', converted_map[key])
+
+print('\nnot converted')
+for key in not_converted_map:
+    print(hex(key), ':', not_converted_map[key])
