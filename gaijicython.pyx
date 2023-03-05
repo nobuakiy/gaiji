bcdf4cdae2e54decfeff370c3447cf2c5c1838bd
Nobuaki Yamashita <nobuakiy09@gmail.com>
2023-03-05 10:56:28 +0900

    fix bug

--- gaijicython.pyx
+++ gaijicython.pyx
@@ -40,59 +40,59 @@ def main_process():
     print("ファイル名, 行数, 外字件数, 変換不可外字件数")
     for filename in file_list:
         out_filename = output_dir + '\\' + filename
+        is_kanji_first = False  # 漢字の1バイト目かどうか
         with open(input_dir + '\\' + filename, "rb") as f:
-            count_gaiji = 0 # 外字の件数
-            not_converted_gaiji = 0 # 変換できなかった外字
-            count_line = 0  # 行数
-            # 1M単位で、一気読み
-            input_buffer = f.read(buff_length)
-            output_buffer = bytearray()
-            
-            is_kanji_first = False  # 漢字の1バイト目かどうか
-            while input_buffer:
-                for ch in input_buffer:
-                    # 前回の文字が漢字の1バイト目なら、今回は2バイト目
-                    if is_kanji_first:
-                        is_kanji_first = False
-                        ch2 = ch
-                        # 外字の範囲かどうか
-                        if gaiji_start <= ch1 and ch1 <= gaiji_end:
-                            count_gaiji += 1
-                            gaiji = (ch1 << 8) + ch2
-                            if gaiji in trans_map:
-                                converted = trans_map[gaiji] 
-                                if gaiji in converted_map:
-                                    converted_map[gaiji] += 1
-                                else:
-                                    converted_map[gaiji] = 1
-                                output_buffer.append((converted >> 8) & 0xff)
-                                output_buffer.append(converted & 0xff)
-                                continue
-                            else:
-                                not_converted_gaiji += 1
-                                if gaiji in converted_map:
-                                    not_converted_map[gaiji] += 1
-                                else:
-                                    not_converted_map[gaiji] = 1
-                        output_buffer.append(ch1)
-                        output_buffer.append(ch2)
-                    # 漢字の1バイト目なら、取っておいて、2バイト目を読んだときに処理
-                    elif (kanji_start1 <= ch and ch <= kanji_end1) or (kanji_start2 <= ch and ch <= kanji_end2):
-                        is_kanji_first = True
-                        ch1 = ch
-                    # 漢字に関係ないならそのまま出力
-                    else:
-                        if ch == CHAR_LF:
-                            count_line += 1
-                        output_buffer.append(ch)
-
-                with open(out_filename, "wb") as fout:
-                    fout.write(output_buffer)
-                output_buffer.clear()
-
+            with open(out_filename, "wb") as fout:
+                count_gaiji = 0 # 外字の件数
+                not_converted_gaiji = 0 # 変換できなかった外字
+                count_line = 0  # 行数
+                # 1M単位で、一気読み
                 input_buffer = f.read(buff_length)
+                output_buffer = bytearray()
+                while input_buffer:
+                    for ch in input_buffer:
+                        # 前回の文字が漢字の1バイト目なら、今回は2バイト目
+                        if is_kanji_first:
+                            is_kanji_first = False
+                            ch2 = ch
+                            # 外字の範囲かどうか
+                            if gaiji_start <= ch1 and ch1 <= gaiji_end:
+                                count_gaiji += 1
+                                gaiji = (ch1 << 8) + ch2
+                                if gaiji in trans_map:
+                                    converted = trans_map[gaiji] 
+                                    if gaiji in converted_map:
+                                        converted_map[gaiji] += 1
+                                    else:
+                                        converted_map[gaiji] = 1
+                                    output_buffer.append((converted >> 8) & 0xff)
+                                    output_buffer.append(converted & 0xff)
+                                    continue
+                                else:
+                                    not_converted_gaiji += 1
+                                    if gaiji in converted_map:
+                                        not_converted_map[gaiji] += 1
+                                    else:
+                                        not_converted_map[gaiji] = 1
+                            output_buffer.append(ch1)
+                            output_buffer.append(ch2)
+                        # 漢字の1バイト目なら、取っておいて、2バイト目を読んだときに処理
+                        elif (kanji_start1 <= ch and ch <= kanji_end1) or (kanji_start2 <= ch and ch <= kanji_end2):
+                            is_kanji_first = True
+                            ch1 = ch
+                        # 漢字に関係ないならそのまま出力
+                        else:
+                            if ch == CHAR_LF:
+                                count_line += 1
+                            output_buffer.append(ch)
 
-            print("{0}, {1}, {2}, {3}".format(filename, count_line, count_gaiji, not_converted_gaiji))
+
+                    fout.write(output_buffer)
+                    output_buffer.clear()
+
+                    input_buffer = f.read(buff_length)
+
+                print("{0}, {1}, {2}, {3}".format(filename, count_line, count_gaiji, not_converted_gaiji))
 
 
     print('\n外字出現件数')
